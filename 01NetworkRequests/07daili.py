6cfccf78b2943ce8852996756a84e837f08a744b
idwyane <dwadeisthebest@163.com>
2018-10-25 17:39:17 +0800

    完成电影天堂简单爬取

--- 01NetworkRequests/07daili.py
+++ 01NetworkRequests/07daili.py
@@ -5,8 +5,11 @@ import requests
 
 
 proxy = {
-    'http':'118.190.95.35:9001'
+    'http':'42.231.251.104:80'
 }
 
 response = requests.get("http://httpbin.org/ip",proxies=proxy)
+response2 = requests.get('https://mp.weixin.qq.com/s/Fk3dxhXnAo0hsYuiOu6fmg',proxies=proxy)
 print(response.text)
+print("="*30)
+print(response2.text)
--- 02DataStorage/dytt_spider.py
+++ 02DataStorage/dytt_spider.py
@@ -9,7 +9,6 @@ BASE_DOMAIN = 'http://www.ygdy8.net'
 
 HEADERS = {
     'User_Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
-    'Referer': 'http://www.ygdy8.net/html/gndy/dyzz/list_23_3.html'
 }
 
 
@@ -43,12 +42,11 @@ def parse_detail_page(url):
     movie = {}
     response = requests.get(url,headers = HEADERS)
     text = response.content.decode('gbk', errors='ignore')
-    print(text)
     html = etree.HTML(text)
-    title = html.xpath("//div[@class='title_all'].//font[@color='#07519a']")
+    # title = html.xpath("//div[@class='title_all']//font[@color='#07519a']")  # 本行47行，下面已修改
 
    # 打印出 [<Element font at 0x10cb422c8>, <Element font at 0x10cb42308>]
-    print(title)
+   #  print(title)
 
     # 为了显示，我们需要转一下编码
     # for x in title:
@@ -61,14 +59,13 @@ def parse_detail_page(url):
     zoomE = html.xpath("//div[@id='Zoom']") [0] # 求出共同的顶级容器，方便后面求职
     imgs = zoomE.xpath(".//img/@src") # 求出海报和截图
     cover = imgs[0]
-    screenshot = imgs[1]
+    if len(imgs) > 1:
+        screenshot = imgs[1]
+        movie['screenshot'] = screenshot
     # print(cover)
     movie['cover'] = cover
-    movie['screenshot'] = screenshot
-
 
     infos = zoomE.xpath(".//text()")
-    # print(infos)
 
     for index,info in enumerate(infos):
         if info.startswith('◎年　　代'):
@@ -83,31 +80,73 @@ def parse_detail_page(url):
         elif info.startswith("◎豆瓣评分"):
             info = info.replace("◎豆瓣评分", "").strip()
             movie["douban_rating"] = info
-        elif info.startswith["◎片　　长"]:
+        elif info.startswith("◎片　　长"):
             info = info.replace("◎片　　长","").strip()
             movie["duration"] = info
         elif info.startswith("◎导　　演"):
             info = info.replace("◎导　　演", "").strip()
             movie["director"] = info
         elif info.startswith("◎主　　演"):
-            info = info.replace("◎主　　演", "").strip()
+            # info = info.replace("◎主　　演", "").strip()
             # 因为主演有很多个，再加上其在电影天堂中元素的特殊性，需要遍历一遍，在分别求出每一个演员
-            for x in range(1,100): # 写100 是因为不知道主演个数，写大点
-                pass
+            actors = []
+            for x in range(index+1,len(infos)): # 从演员 infos 开始遍历，求出每一个演员
+                actor = infos[x].strip()
+                if actor.startswith("◎"): # 也就是到了标签 的 ◎ 就退出
+                    break
+                actors.append(actor)
+            movie['actor'] = actors
+        elif info.startswith('◎简　　介 '):
 
+            # info = info.replace('◎简　　介 ',"").strip()
+            for x in range(index+1,len(infos)):
+                if infos[x].startswith("◎获奖情况"):
+                  break
+                profile = infos[x].strip()
+                movie['profile'] = profile
+            # print(movie)
+        elif info.startswith('◎获奖情况 '):
+            awards = []
+            # info = info.replace("◎获奖情况 ", "").strip()
+            for x in range(index+1,len(infos)):
+                if infos[x].startswith("【下载地址】"):
+                    break
+                award = infos[x].strip()
+                awards.append(award)
+            movie['awards'] = awards
+            # print(awards)
 
+    download_url = html.xpath("//td[@bgcolor='#fdfddf']/a/@href")[0]
+    movie['download_url'] = download_url
+    return  movie
 
 def spider():
     base_url = 'http://www.ygdy8.net/html/gndy/dyzz/list_23_{}.html'
+    # movies = [] # 前7页的总数据
     # 第一个for循环是为了爬取前7页
     for x in range(1,8):
-        url = base_url.format(x)
-        detail_urls = get_detail_urls(url)
+        url = base_url.format(x) # 求出每一页电影列表的url eg: http://www.ygdy8.net/html/gndy/dyzz/list_23_1.html
+
+        detail_urls = get_detail_urls(url) # 拿到列表中每一步电影的详情页的 url
+        movies = []
         # 第二个for循环是为了爬取每一个电影详情页
         for detail_url in detail_urls:
             movie = parse_detail_page(detail_url)
-            break
-        break
+            movies.append(movie)
+        print('第%d页, %s\n\n' % (x, movies)) # 分别打印前7页的详情页
+
+# 存储写此代码时的html
+def store_url():
+
+    url = 'http://www.ygdy8.net/html/gndy/dyzz/20181022/57644.html'
+    # return
+    resp =  requests.get(url,headers=HEADERS)
+    # print(resp.json())
+
+    with open('movie.html','w',encoding='gbk') as fp:
+         fp.write(resp.content.decode('gbk', errors='ignore'))
+    print("搞定")
 
 if __name__ == '__main__':
     spider()
+    # store_url()
