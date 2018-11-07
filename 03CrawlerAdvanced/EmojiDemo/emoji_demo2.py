7c917417ab6fe6a68d15ef8847ccf796b9a7df6b
iwade <583205004@qq.com>
2018-11-08 01:15:39 +0800

    多线程爬取表情包（完成）
    同步和多线程的demo都在 EmojiDemo 里面

--- 03CrawlerAdvanced/EmojiDemo/emoji_demo2.py
+++ 03CrawlerAdvanced/EmojiDemo/emoji_demo2.py
@@ -19,9 +19,11 @@ class Producer(threading.Thread):
         super(Producer, self).__init__(*args,**kwargs)
         self.page_queue = page_queue
         self.img_queue = img_queue
+
     def run(self):
         while True:
-
+            if self.page_queue.empty():
+                break
             url = self.page_queue.get() # 获取 url
             self.parse_page(url)
 
@@ -37,12 +39,12 @@ class Producer(threading.Thread):
             # print(img_url)
             alt = img.get('alt')  # 获取图片名字
             # alt 可能某些情况下需要处理非法字符（这些字符不可以当做名字保存）
-            print(alt)
+
             suffix = os.path.splitext(img_url)[1]  # 对url进行分割，取数组中的第二位，得到后缀名
             filename = alt + suffix
-            print(filename)
-            self.img_queue.put(img_url,filename)
-            # request.urlretrieve(img_url, 'images/' + filename) # 保存图片
+            # print(filename)
+            self.img_queue.put(img_url) # 注意队列的存取操作： 先进先出
+            self.img_queue.put(filename)
 
 # 消费者
 class Consumer(threading.Thread):
@@ -52,16 +54,37 @@ class Consumer(threading.Thread):
         super(Consumer, self).__init__(*args,**kwargs)
         self.page_queue = page_queue
         self.img_queue = img_queue
-    def run(self):
+        # print("img地址：%s" % self.img_queue)
+        # print("page地址 %s" % self.page_queue)
 
+    def run(self):
+        while True:
+
+            # 如果图片队列和页面队列都为空，page_queue证明没数据，就退出队列，线程也会自动退出
+            if self.img_queue.empty() and self.page_queue.empty():
+                break
+            img_url = self.img_queue.get() # 注意队列的存取操作： 先进先出
+            filename=  self.img_queue.get()
+            # print(img_url)
+            request.urlretrieve(img_url, 'images/'+filename)
+            print(filename+ "下载完成！")
 
 def main():
     page_queue = Queue(100)
     img_queue = Queue(500)
     for x in range(1,51): # 爬取前50页 range(1,3) 这里相当于 1 2
-        url = 'https://www.doutula.com/photo/list/?page=%d' % x
 
-        # break
+        url = 'https://www.doutula.com/photo/list/?page=%d' % x
+        # print(url)
+        page_queue.put(url)
+
+    for x in range(5):
+        t = Producer(page_queue,img_queue)
+        t.start()
+
+    for x in range(5):
+        t = Consumer(page_queue,img_queue)
+        t.start()
 
 if __name__ == '__main__':
     main()
