8aba55109394c7698d3cae8b33aebdd42ae181b3
iwade <583205004@qq.com>
2018-11-14 01:37:48 +0800

    拉勾爬取完成

--- 03CrawlerAdvanced/ajax_spider_demo/lagou_spider/lagou2.py
+++ 03CrawlerAdvanced/ajax_spider_demo/lagou_spider/lagou2.py
@@ -5,21 +5,30 @@ from lxml import etree
 import re
 import time
 from selenium.webdriver.support.ui import Select,WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class LagouSpider(object):
     positions = [] # 用于存储
-    driver_path = r'/Users/Dwyane/Documents/pycharm/chromedriver'  # 加 r 表示原生字符串
+    driver_path = r'/Users/aaa/Downloads/chromedriver'  # 加 r 表示原生字符串
     def __init__(self):
         self.driver = webdriver.Chrome(executable_path=LagouSpider.driver_path)
         self.url= 'https://www.lagou.com/jobs/list_python?labelWords=&fromSearch=true&suginput='
 
     def run(self):
         self.driver.get(self.url)
+
         while True:
             source = self.driver.page_source
-            self.parse_list_page(source) # 读取每一页的html源码，并得出详情页的 url
 
-            # try catch : 如果报错，则立即打印 source 源码
+            # 等有下一页的按钮出现，才执行其他操作
+            WebDriverWait(driver=self.driver,timeout=10).until(
+                EC.presence_of_all_elements_located((By.XPATH,"//div[@class='pager_container']//span[@action='next']"))
+            )
+
+            self.parse_list_page(source)  # 读取每一页的html源码，并得出详情页的 url，解析每一页的所有详情页
+
+            # try catch : #如果报错，则立即打印 source 源码
             try:
                 # 获取下一页的按钮
                 nextBtn = self.driver.find_element_by_xpath("//div[@class='pager_container']//span[@action='next']")
@@ -37,25 +46,46 @@ class LagouSpider(object):
     # 读取每一页的html源码，并得出详情页的 url
     def parse_list_page(self,source):
         html = etree.HTML(source)
+
+        # 所有页数的每一页的每一个招聘的详情页url
         links = html.xpath("//div[@class='position']//@href")
+        print(links)
+        # 求出每一页的所有详情页url
         for link in links:
             self.request_detail_page(link)
-            time.sleep(1)
-        print(self.positions) # 打印所有的数据
 
-    # 请求详情页
+
+    # 打开详情页
     def request_detail_page(self,url):
-        self.driver.get(url)
+
+        # 打开详情页，同时切换窗口
+        self.driver.execute_script("window.open('%s')" % url)
+        self.driver.switch_to.window(self.driver.window_handles[1])
+
+        WebDriverWait(driver=self.driver, timeout=10).until(
+            # 去掉 /text() ，因为 presence_of_all_elements_located 没法找到 文本信息，只能找到元素
+            EC.presence_of_all_elements_located((By.XPATH,"//span[@class='name']"))
+        )
+
+        # 解析详情页
         source = self.driver.page_source
         self.parse_detail_page(source)
 
+        # 关闭当前详情页，并切换到职位列表页
+        self.driver.close()
+        self.driver.switch_to.window(self.driver.window_handles[0])
+
+
     # 解析详情页，利用 xpath
     def parse_detail_page(self,source):
+
         html = etree.HTML(source)
+
         position_name = html.xpath("//span[@class='name']/text()")[0]
-        # print(position_name)
 
         job_request_spans = html.xpath("//dd[@class='job_request']//span")
+
+
         salary = job_request_spans[0].xpath('.//text()')[0].strip()  # .strip() 去掉空格
         # print(salary)
 
@@ -72,9 +102,11 @@ class LagouSpider(object):
         education = re.sub(r"[\s/]", "", education)
         # print(education)
 
+        company_name = html.xpath("//h2[@class='fl']/text()")[0].strip()
         # 存储数据
         position = {
             'name':position_name,
+            'company_name':company_name,
             'salary':salary,
             'city':city,
             'work_years':work_years,
